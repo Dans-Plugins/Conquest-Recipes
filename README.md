ad1d6e61ea0f6e7d56bab44b5ebb7e795f114bba
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-24 14:09:03 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Conquest-Recipes" 
+"# Conquest-Recipes" 
---
+++ src/main/java/dansplugins/conquestrecipes/ConquestRecipes.java
@@ -0,0 +1,17 @@
+package dansplugins.conquestrecipes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ConquestRecipes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
