92d2910fad5c85dd259bba621c371be9e017dd3d
dmccoystephenson <dmccoystephenson@gmail.com>
2020-08-15 02:25:29 -0600

    added a handler for crafting

---
+++ src/main/java/conquestrecipesystem/EventHandlers/PrepareItemCraftEventHandler.java
@@ -0,0 +1,36 @@
+package conquestrecipesystem.EventHandlers;
+
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class PrepareItemCraftEventHandler {
+
+    public void handle(PrepareItemCraftEvent event) {
+
+        // if result has no lore
+        if (!hasLore(event.getRecipe().getResult())) {
+            Recipe recipe = event.getRecipe();
+            Inventory craftingInventory = event.getInventory();
+            for (ItemStack item : craftingInventory) {
+                if (hasLore(item)) {
+                    // TODO: prevent crafting
+                }
+            }
+        }
+
+    }
+
+    public boolean hasLore(ItemStack item) {
+        if (item.hasItemMeta()) {
+            ItemMeta meta = item.getItemMeta();
+            if (meta.hasLore()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
--- src/main/java/conquestrecipesystem/Main.java
+++ src/main/java/conquestrecipesystem/Main.java
@@ -1,13 +1,18 @@
 package conquestrecipesystem;
 
+import conquestrecipesystem.EventHandlers.PrepareItemCraftEventHandler;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import conquestrecipesystem.Subsystems.CommandSubsystem;
 import conquestrecipesystem.Subsystems.ItemStackSubsystem;
 import conquestrecipesystem.Subsystems.RecipeSubsystem;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
 
     public static String version = "v0.5";
 
@@ -29,5 +34,11 @@ public final class Main extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         CommandSubsystem commandInterpreter = new CommandSubsystem(this);
         return commandInterpreter.interpretCommand(sender, label, args);
+    }
+
+    @EventHandler()
+    public void onCraft(PrepareItemCraftEvent event) {
+        PrepareItemCraftEventHandler handler = new PrepareItemCraftEventHandler();
+        handler.handle(event);
     }
 }
