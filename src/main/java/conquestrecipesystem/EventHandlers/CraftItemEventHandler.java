29818593e7701f972e6d2e1656f46738186060d8
dmccoystephenson <dmccoystephenson@gmail.com>
2020-08-15 17:18:18 -0600

    added whitelisted items

--- src/main/java/conquestrecipesystem/EventHandlers/CraftItemEventHandler.java
+++ src/main/java/conquestrecipesystem/EventHandlers/CraftItemEventHandler.java
@@ -6,6 +6,8 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import static org.bukkit.Material.PAPER;
+
 public class CraftItemEventHandler {
 
     public void handle(CraftItemEvent event) {
@@ -13,16 +15,18 @@ public class CraftItemEventHandler {
         // if result has no lore
         ItemStack craftedItem = event.getInventory().getResult();
         if (craftedItem != null) {
-            if (!hasLore(craftedItem)) {
+            if (!isWhitelisted(craftedItem)) {
+                if (!hasLore(craftedItem)) {
 //                System.out.println("Result does not have lore!");
-                Inventory craftingInventory = event.getInventory();
-                for (ItemStack item : craftingInventory) {
-                    if (item != null) {
-                        if (hasLore(item)) {
+                    Inventory craftingInventory = event.getInventory();
+                    for (ItemStack item : craftingInventory) {
+                        if (item != null) {
+                            if (hasLore(item)) {
 //                            System.out.println("Ingredients did have lore!");
-                            event.setCancelled(true);
-                            event.getWhoClicked().sendMessage(ChatColor.RED + "Cannot use custom items in a regular crafting recipe!");
-                            return;
+                                event.setCancelled(true);
+                                event.getWhoClicked().sendMessage(ChatColor.RED + "Cannot use custom items in a regular crafting recipe!");
+                                return;
+                            }
                         }
                     }
                 }
@@ -44,6 +48,22 @@ public class CraftItemEventHandler {
             }
         }
 //        System.out.println("Item does not have lore!");
+        return false;
+    }
+
+    // to make this compatible with food spoilage
+    public boolean isWhitelisted(ItemStack item) {
+        switch (item.getType()) {
+            case BREAD:
+            case MUSHROOM_STEW:
+            case RABBIT_STEW:
+            case BEETROOT_SOUP:
+            case CAKE:
+            case PUMPKIN_PIE:
+            case SUGAR:
+            case COOKIE:
+                return true;
+        }
         return false;
     }
 
